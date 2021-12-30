53ca8f8df44f1506edc7837bf556f1a454facc9e
kamocyc <kamocyc@users.noreply.github.com>
2021-12-30 16:17:12 +0900

    minor

---
+++ benchmark/check_update_by_simplification.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for ORGFILE in $(cat benchmark/file_list/all_paper.txt)
+do
+  FILE=benchmark/inputs/$ORGFILE
+  echo "$FILE"
+  dune exec bin2/simplify.exe -- --show-style abbrev --agg "$FILE" > out1.txt
+  res=$(diff "$FILE" "$FILE"_cp.in)
+  if [ "$res" ]; then
+    echo "CHANGED: $FILE"
+  fi
+done
