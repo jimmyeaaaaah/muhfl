97d923019c2f5c7b7d32166b9173b84fdb577f57
kamocyc <kamocyc@users.noreply.github.com>
2022-02-03 13:39:54 +0900

    mufu

--- lib/muapprox.ml
+++ lib/muapprox.ml
@@ -57,7 +57,7 @@ let parse_file_with_mufu file =
   Log.info begin fun m -> m ~header:"Input" "mufu transformation start" end;
   let raw, s = MuFU_core.transform raw env in
   Log.info begin fun m -> m ~header:"Input" "mufu transformation end" end;
-  Log.info begin fun m -> m ~header:"Input" "mufu transformation result: %s" s end;
+  Log.app begin fun m -> m ~header:"Input" "mufu transformation result: %s" s end;
   Raw_hflz.Typing.to_typed raw
   |> (fun (e, rules) -> e, List.map ~f:Raw_hflz.rename_simple_ty_rule rules)
   |> Raw_hflz.rename_ty_body
